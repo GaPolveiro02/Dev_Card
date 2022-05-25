86e1fb1d7472f113dcc5aafa25aa84d559b68637
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-05-25 15:15:31 -0300

    Added fuction to hide repositories Card

--- main.js
+++ main.js
@@ -13,30 +13,71 @@ function searchUser() {
 }
 
 function getUser(user) {
-    fetch(`${url}${user}`)
+    let searchUrl = `${url}${user}`;
+
+    fetch(searchUrl)
      .then(response => response.json())
      .then(data => {
             userName.textContent = data.name
             devUser.textContent = data.login
+            githubLink.href = data.html_url
             userAvatar.src = data.avatar_url
             userRepos.textContent = `Public repositories ${data.public_repos}`
             userFollowers.textContent = `Followers ${data.followers}`
             userFollowing.textContent = `Following ${data.following}`
+
             if(data.location != null){
                 userLocation.textContent = `Location: ${data.location}`
             }
             else{
-                userLocation.textContent = ``
+                userLocation.textContent = `?`
             }
-        })
-     .catch(error => console.log(error))
-}
 
+            getUserRepos(searchUrl)
+        })
+     .catch(error => console.log(error));
+}
 
 function getPerfilColor() {
     const color = '#' + Math.floor(Math.random()*16777215).toString(16);
-    console.log(color) 
+
     document.documentElement.style.setProperty('--color', `${color}`)
+}
+
+function getUserRepos(searchUrl) {
+    const reposUrl = `${searchUrl}/repos`
+
+    console.log(reposUrl)
+
+    fetch(reposUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(repo => {
+            let li = document.createElement('li')
+            li.setAttribute('class','item')
+            li.innerHTML = 
+            `<a href="${repo.html_url}" target="_blank">
+                <img src="./assets/img/repository.svg" alt="Repositórios">
+                ${repo.name}
+            </a>`
+
+            reposUl.append(li)
+        });
+    })
+}
+
+function hideRepos() {
+    const reposCard = document.querySelector('#reposCard')
+    
+    console.log(reposCard.style)
+
+    if(reposCard.style.display == '') {
+        reposCard.style.display = 'flex'
+    }else if(reposCard.style.display == 'none') {
+        reposCard.style.display = 'flex'
+    }else {
+        reposCard.style.display = 'none'
+    }
 }
 
 getUser("gapolveiro02")
